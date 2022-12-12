24dd2603a7ac4d58c0b847d17906df862ca0f056
Ricky Bruno <ricardobruno_89@hotmail.com>
2022-12-12 14:25:26 -0300

    fix layout props

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Footer } from "./Footer";
 import Sidebar from "./Sidebar";
 
-export const Layout = ({ navigation, settings, children }) => {
+export const Layout = ({ navigation, children }) => {
   return (
     <div className="text-slate-700">
-      <Sidebar navigation={navigation} settings={settings} />
+      <Sidebar navigation={navigation} />
       <div className="ml-60 min-h-screen">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
         <Footer />
