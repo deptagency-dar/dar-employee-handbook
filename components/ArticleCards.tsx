bf5ca8ce3482578b02102a39d1f1503ae60bb692
jgarciatau <flacs5@gmail.com>
2023-01-10 09:16:21 -0300

    styling

--- components/ArticleCards.tsx
+++ components/ArticleCards.tsx
@@ -9,9 +9,9 @@ const ArticleCards = (props: Props) => {
   return (
       <div className="container my-12 mx-auto px-4 md:px-12">
         <h1 className="my-3 text-3xl font-semibold tracking-tighter text-slate-800 md:text-4xl">Last articles</h1>
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 justify-items-center lg:justify-items-stretch">
           {articles.map((article) => (
-            <div key={article.title} className="w-full">
+            <div key={article.title}>
               <ArticleCard {...article} />
             </div>
           ))}
--- lib/layoutProvider.tsx
+++ lib/layoutProvider.tsx
@@ -1,33 +1,47 @@
 import { Layout } from "@components/layout";
 import SpinnerArea from "@components/SpinnerArea";
-import React, { useContext } from "react";
-import useSWR, { Fetcher } from 'swr';
+import React, { useContext, useState } from "react";
+import useSWR, { Fetcher } from "swr";
 import { MenuDocument } from "types/types.generated";
 
+interface SidebarState {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+}
 interface ILayoutContext {
+  sidebarState: SidebarState;
   menuDocument: MenuDocument | null;
 }
 
 interface ILayoutResponse {
-  menu: MenuDocument
-};
+  menu: MenuDocument;
+}
 
 const LayoutContext = React.createContext<ILayoutContext>({
+  sidebarState: {} as SidebarState,
   menuDocument: null,
 });
 
 export const useLayoutContext = () => useContext(LayoutContext);
 
 const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
-  const fetcher: Fetcher<ILayoutResponse, string> = (...args) => fetch(...args).then(res => res.json());  
-  const { data } = useSWR<ILayoutResponse>('/api/layout', fetcher);
-  
+  const fetcher: Fetcher<ILayoutResponse, string> = (...args) =>
+    fetch(...args).then((res) => res.json());
+  const { data } = useSWR<ILayoutResponse>("/api/layout", fetcher);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <LayoutContext.Provider value={{ menuDocument: data?.menu || null }}>
+    <LayoutContext.Provider
+      value={{
+        menuDocument: data?.menu || null,
+        sidebarState: {
+          open: sidebarOpen,
+          setOpen: setSidebarOpen,
+        } as SidebarState,
+      }}
+    >
       <SpinnerArea>
-        <Layout>
-          {children}
-        </Layout>
+        <Layout>{children}</Layout>
       </SpinnerArea>
     </LayoutContext.Provider>
   );
